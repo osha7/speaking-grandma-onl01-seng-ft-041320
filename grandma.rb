845cdff5321775e514f22dcfdaf4dbd6168812ac
Jennifer Groetz <ogroetz@gmail.com>
2020-04-14 11:32:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,9 +16,9 @@ def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN"
   elsif phrase = phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+    return "NO, NOT SINCE 1938!"
   else
-    return "NO! NOT SINCE 1938!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
