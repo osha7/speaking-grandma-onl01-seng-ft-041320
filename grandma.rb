3bc5db02614aba7c4ad6febf7edb4d0e8ff4c2a6
Jennifer Groetz <ogroetz@gmail.com>
2020-04-14 11:29:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if(phrase =="I LOVE YOU GRANDMA!")
+  if phrase == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN"
-  elseif(phrase=phrase.upcase)
+  elseif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO! NOT SINCE 1938!"
