ff34e1df18ad94e425abb5d3c1a19c62f488cb93
Jennifer Groetz <ogroetz@gmail.com>
2020-04-14 11:42:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
   elsif (phrase==phrase.downcase)
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH?! SPEAK UP, SONNY!"
+    return "NO, NOT SINCE 1938!"
   end
 end
 
